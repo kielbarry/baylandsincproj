0737fe336c5682a9e46b4f9587c5cf6349f4f6b2
Kiel Barry <kielbarry@Kiels-MacBook-Pro.local>
2018-04-11 17:27:23 -0700

    auth route wip

--- backendFunctions/auth.js
+++ backendFunctions/auth.js
@@ -4,22 +4,16 @@ const
 	jwt = require("jsonwebtoken"),
 	jwtToken = process.env.JWT_TOKEN;
 
-// module.exports = {
-// 	vjt: verifyJwtToken
-// }
+
 module.exports = (req, res, next) => {
 	try {
-		const decoded = jwt.verify(req.body.token, jwtToken, null)
+		const b = req.headers.authorization.split(" ")[1]	
+		const decoded = jwt.verify(b, jwtToken, null)
+		console.log("jwt verify result", )
+		req._userData = decoded;
+		next();
 	} catch(error) {
+		console.log("error jwt", error)
 		return res.status(401).json({message: "auth failure"})
 	}
 }
-
-// function verifyJwtToken(req, res, next) {
-// 	try {
-// 		const decoded = jwt.verify(req.body.token, jwtToken, null)
-// 	} catch(error) {
-// 		return res.status(401).json({message: "auth failure"})
-// 	}
-
-// }
