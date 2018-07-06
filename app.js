4dbee58eda5dd08f0005b62a434128ac1d4ee6cc
Kiel barry <kiel.j.barry@gmail.com>
2018-07-06 15:08:09 -0700

    ,m

--- app.js
+++ app.js
@@ -9,38 +9,38 @@ path = require("path"),
 PORT = process.env.PORT || 3000,
 bodyParser = require("body-parser"),
 request = require("request"),
-URL = require("url-parser"),
+URL = require("url-parser");
 
-initDb = require("./db/init.js"),
-pg = require('pg'),
-{ Client } = require('pg'),
-username = process.env.PGUSER,
-password = process.env.PGPASSWORD,
-host = process.env.PGHOST,
-database = process.env.PGDATABASE,
-port = process.env.PGPORT,
-connectionString = process.env.DATABASE_URL || `postgres://${username}:${password}@${host}:${port}/${database}`,
-client = new pg.Client(connectionString);{}
-
-
+// initDb = require("./db/init.js"),
+// pg = require('pg'),
+// { Client } = require('pg'),
+// username = process.env.PGUSER,
+// password = process.env.PGPASSWORD,
+// host = process.env.PGHOST,
+// database = process.env.PGDATABASE,
+// port = process.env.PGPORT,
+// connectionString = process.env.DATABASE_URL || `postgres://${username}:${password}@${host}:${port}/${database}`,
+// client = new pg.Client(connectionString);{}
 
 
-salt = process.env.BCRYPT_SALT,
-jwt = require("jsonwebtoken"),
-jwtToken = process.env.JWT_TOKEN,
-checkAuth = require("./backendFunctions/auth.js"),
-redis = require("redis"),
-redisPort = process.env.REDIS_PORT,
-redisHost = process.env.REDIS_HOST,
-redisClient = redis.createClient(redisPort, redisHost),
-handler = require("./backendFunctions/handlers.js"),
-nexmoHandler = require("./backendFunctions/nexmoHandlers.js"),
-rcsk = process.env.recaptchaSecret;
 
 
-client.connect()
+// salt = process.env.BCRYPT_SALT,
+// jwt = require("jsonwebtoken"),
+// jwtToken = process.env.JWT_TOKEN,
+// checkAuth = require("./backendFunctions/auth.js"),
+// redis = require("redis"),
+// redisPort = process.env.REDIS_PORT,
+// redisHost = process.env.REDIS_HOST,
+// redisClient = redis.createClient(redisPort, redisHost),
+// handler = require("./backendFunctions/handlers.js"),
+// nexmoHandler = require("./backendFunctions/nexmoHandlers.js"),
+// rcsk = process.env.recaptchaSecret;
 
-initDb.initQuery();
+
+// client.connect()
+
+// initDb.initQuery();
 
 
 // handler.getExchangeHoldings();
@@ -60,63 +60,63 @@ http
 	.on("listening", () => console.log("serving port: ", PORT))
 
 
-redisClient.on('connect', (err, res) => err ? console.log(err) : console.log('redis connected'))
+// redisClient.on('connect', (err, res) => err ? console.log(err) : console.log('redis connected'))
 
-app.put("/newEmail", (req, res) => {
-	if (!req.body) return res.sendStatus(400)
-	redisClient.HEXISTS("emailList", req.body.email, (err, exists) => {
-		exists === 1 ? redisClient.HGET("emailList", req.body.email) : res.json({"valid": "true"})
-	})
-})
+// app.put("/newEmail", (req, res) => {
+// 	if (!req.body) return res.sendStatus(400)
+// 	redisClient.HEXISTS("emailList", req.body.email, (err, exists) => {
+// 		exists === 1 ? redisClient.HGET("emailList", req.body.email) : res.json({"valid": "true"})
+// 	})
+// })
 
-app.post("/signup", (req, res) => {
-	if (!req.body) return res.sendStatus(400)
-	nexmoHandler.signUp(req, res)
-});
+// app.post("/signup", (req, res) => {
+// 	if (!req.body) return res.sendStatus(400)
+// 	nexmoHandler.signUp(req, res)
+// });
 
-app.put("/cancelNexmo", (req, res) => {
-	nexmoHandler.cancel(req, res)
-})
+// app.put("/cancelNexmo", (req, res) => {
+// 	nexmoHandler.cancel(req, res)
+// })
 
-app.put("/verifyNexmo", (req, res) => {
-	nexmoHandler.verify(req, res)
-})
+// app.put("/verifyNexmo", (req, res) => {
+// 	nexmoHandler.verify(req, res)
+// })
 
-app.put("/getNewNexmoCode", (req, res) => {
-	nexmoHandler.getNewNexmoCode(req, res)
-})
+// app.put("/getNewNexmoCode", (req, res) => {
+// 	nexmoHandler.getNewNexmoCode(req, res)
+// })
 
-app.put("/login", (req, res) => {
-	if(!req.body) return res.sendStatus(400)
-	checkAuth.login(req,res);
-})
+// app.put("/login", (req, res) => {
+// 	if(!req.body) return res.sendStatus(400)
+// 	checkAuth.login(req,res);
+// })
 
 
 
-app.put("/getBalances", (req, res) => {
-	if (!req.body) return res.sendStatus(400)
-	handler.getExchangeHoldings(req, res)
+// app.put("/getBalances", (req, res) => {
+// 	if (!req.body) return res.sendStatus(400)
+// 	handler.getExchangeHoldings(req, res)
 
-// .then(resp => res.send(resp))
-})
+// // .then(resp => res.send(resp))
+// })
 
-app.get("/getAllHoldings/:id/:versionid", (req, res) => {
-	if (!req.body) return res.sendStatus(400)
+// app.get("/getAllHoldings/:id/:versionid", (req, res) => {
+// 	if (!req.body) return res.sendStatus(400)
 
-	console.log(req.params.id, req.params.versionid)
+// 	console.log(req.params.id, req.params.versionid)
 
-})
+// })
 
-app.get("/getMinAndMaxCoinPrice", (req, res) => {
+// app.get("/getMinAndMaxCoinPrice", (req, res) => {
 
-	// console.log(handler.getGDAXPrices());
-	// handler.getPoloniexPrices().then(e=>console.log(e))
-	// handler.getKrakenPrices();
-	// handler.getBinancePrices().then(e=>console.log(e))
+// 	// console.log(handler.getGDAXPrices());
+// 	// handler.getPoloniexPrices().then(e=>console.log(e))
+// 	// handler.getKrakenPrices();
+// 	// handler.getBinancePrices().then(e=>console.log(e))
 
-	// repeater(getAllandSend(res), 10000)
-	handler.getAllPrices().then(resp => {
-		res.send(resp)
-	})
-	// .then(e => console.log(e["poloniex"]))
-});
+// 	// repeater(getAllandSend(res), 10000)
+// 	handler.getAllPrices().then(resp => {
+// 		res.send(resp)
+// 	})
+// 	// .then(e => console.log(e["poloniex"]))
+// });
